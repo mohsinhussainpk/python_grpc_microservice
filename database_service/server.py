f3bb7295198254aa741ff7d439d3060baf571a1c
mohsin.hussain <mohsin.hussain@int.sybrid.com>
2022-04-25 11:55:59 +0500

    environment variable with mongodb and get request

--- database_service/server.py
+++ database_service/server.py
@@ -1,7 +1,26 @@
 from concurrent import futures
 import random
-
+import pymongo
+from dotenv import load_dotenv
+import os
 import grpc
+
+load_dotenv()
+
+MONGO_DB_URL = os.getenv('MONGO_DB_URL')
+print(MONGO_DB_URL)
+client = pymongo.MongoClient(MONGO_DB_URL)
+mydb = client.pythonGRPC
+print(mydb)
+
+mycol = mydb["Boxes"]
+
+mydict = { "name": "box1", "id": 2 , "price": 20, "description": 'description text', "category" :'cat', "quantity": 9 }
+#x = mycol.insert_one(mydict)
+x = mycol.find_one()
+print(x)
+print(x['name'])
+
 
 from db_pb2 import (
     Box,
@@ -17,6 +36,7 @@ from db_pb2 import (
     DeleteBoxResponse,
     GetBoxesInCategoryRequest,
     GetBoxesInTimeRangeRequest,
+    RequestStatus,
 )
 
 import db_pb2_grpc
@@ -26,12 +46,13 @@ import db_pb2_grpc
 # Box(name='box2', id=3, price=20, description='disce', category='cate', quantity=10)
 # ]
 
-books_by_category = Box(name='box1', id=2, price=20, description='disc', category='cat', quantity=9)
     # BookCategory.MYSTERY: [
     #     BookRecommendation(id=1, title="The Maltese Falcon"),
     #     BookRecommendation(id=2, title="Murder on the Orient Express"),
     #     BookRecommendation(id=3, title="The Hound of the Baskervilles"),
     # ],
+
+
   
   
 
@@ -41,6 +62,19 @@ class DatabaseService(
     db_pb2_grpc.DatabaseServiceServicer
 ):
     def GetBox(self, request, context):
+        x = mycol.find_one()
+
+        box_details = Box(name=x['name'], id=x['id'], price=x['price'], description=x['description'], category=x['category'], quantity=x['quantity'])
+
+
+
+        return GetBoxResponse(box=box_details)
+
+    def CreateBox(self, request, context):
+        #metadata = dict(context.invocation_metadata())
+        #print(metadata)
+        box = Box(name=request.name, id=1, price=request.price, description=request.description, category=request.category, quantity=request.quantity)
+        #CreateBoxRequest(box=box)
         #if request.name not in books_by_category:
         #   context.abort(grpc.StatusCode.NOT_FOUND, "Category not found")
         #print(books_by_category)
@@ -49,8 +83,11 @@ class DatabaseService(
         # books_to_recommend = random.sample(
         #     books_for_category
         # )
+        #x = mycol.insert_one(books_by_category)
+        #print(request)
 
-        return GetBoxResponse(box=books_by_category)
+        return CreateBoxResponse(box=box)
+
         #return books_by_category
 
     # def GetBox(self, request, context):
